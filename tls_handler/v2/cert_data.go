14050b995876ca2b9119fe6362a5af3552c21b9b
Blackstartech <blackstartech254@gmail.com>
2024-06-04 23:26:46 +0300

    simple example tls certificate and ca certificate

--- tls_handler/v2/cert_data.go
+++ tls_handler/v2/cert_data.go
@@ -1,8 +1,12 @@
 package tlshandler
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"crypto/x509/pkix"
+	"math/big"
 	"net"
+	"time"
 )
 
 type CertData struct {
@@ -32,4 +36,56 @@ func GetSubject(cd CertData) pkix.Name {
 		StreetAddress: []string{cd.StreetAddress},
 		PostalCode:    []string{cd.PostalCode},
 	}
+}
+
+func ServerTLSConf(serverCert tls.Certificate) (serverTLSConf *tls.Config) {
+	serverTLSConf = &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+	}
+
+	return
+}
+
+func ClientTLSConf(caPems ...CAPem) (clientTLSConf *tls.Config) {
+
+	certpool := x509.NewCertPool()
+	for _, pem := range caPems {
+		certpool.AppendCertsFromPEM(pem.PEM)
+	}
+	clientTLSConf = &tls.Config{
+		RootCAs: certpool,
+	}
+
+	return
+}
+
+func ExampleCACert(ca_data CertData) (CA_X509 x509.Certificate) {
+
+	CA_X509 = x509.Certificate{
+		SerialNumber:          big.NewInt(2019),
+		Subject:               GetSubject(ca_data),
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(ca_data.NotAfter.Years, ca_data.NotAfter.Months, ca_data.NotAfter.Days),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+	return
+}
+
+func ExampleTLSCert(ca_data CertData) (CA_X509 x509.Certificate) {
+
+	CA_X509 = x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject:      GetSubject(ca_data),
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(ca_data.NotAfter.Years, ca_data.NotAfter.Months, ca_data.NotAfter.Days),
+
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+	return
 }
